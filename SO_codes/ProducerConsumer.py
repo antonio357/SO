2fa83424e2a3061c50e1a2c287790982fc14fb2f
antonio357 <carlosalbuquerque359@gmail.com>
2019-09-15 13:37:06 -0300

    espera ociosa consertada

--- SO_codes/ProducerConsumer.py
+++ SO_codes/ProducerConsumer.py
@@ -11,34 +11,42 @@ buffer = []
 to solve this problem it is needed a Condition Object to be associated to a Lock Object
 """
 mutex = threading.Lock()
-threading.Condition(mutex)
+condition = threading.Condition(mutex)
 
 
 def fill_buffer():
 
     while True:
-
+        mutex.acquire()
+        
         if len(buffer) < BUFFER_SIZE:
-            mutex.acquire()
             value = randint(0, 9)
             buffer.append(value)
-            sleep(2)
             print("Produced ", value, ", len(buffer) = ", len(buffer))
-
-            mutex.release()
+            condition.notify()
+            
+        else:
+            condition.wait()
+            
+        mutex.release() 
+        sleep(1)
 
 
 def consume_buffer():
 
     while True:
+        mutex.acquire()
 
         if len(buffer) > 0:
-            mutex.acquire()
             value = buffer.pop(0)
-            sleep(2)
             print("Consumed ", value, ", len(buffer) = ", len(buffer))
-
-            mutex.release()
+            condition.notify()
+            
+        else:
+            condition.wait()
+            
+        mutex.release()
+        sleep(2)
 
 
 producer = threading.Thread(target=fill_buffer)
