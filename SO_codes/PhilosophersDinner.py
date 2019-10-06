3f043d68260588a6ec69d1562e8e3334f8de4cbe
antonio357 <carlosalbuquerque359@gmail.com>
2019-10-06 15:48:13 -0300

    dinnig philosophers done

--- SO_codes/PhilosophersDinner.py
+++ SO_codes/PhilosophersDinner.py
@@ -1,43 +1,62 @@
 from threading import Lock, Condition, Thread
 from time import sleep
 
+EATING = 2
+HUNGRY = 1
+THINKING = 0
+
+N = 5
+philosophers_states = []
+philosophers_threads = []
+
 mutex = Lock()
 condition = Condition(mutex)
 
-def think(name):
-    mutex.acquire()
-    print("philosopher ", name, " is thinking")
-    mutex.release()
 
-def eat(name):
-    mutex.acquire()
-    print("philosopher ", name, " is eating")
-    mutex.release()
+def think(index):
+    condition.acquire()
+    philosophers_states[index] = HUNGRY
+    condition.release()
 
-def take_forks(name, left_fork, right_fork):
-    mutex.acquire()
-    print("philosopher ", name, " is taking forks = ", [left_fork, right_fork])
-    mutex.release()
 
-def put_forks(name, left_fork, right_fork):
-    mutex.acquire()
-    print("philosopher ", name, " is putting forks = ", [left_fork, right_fork])
-    mutex.release()
+def eat():
+    condition.acquire()
+    print(philosophers_states)
+    sleep(1)
+    condition.release()
 
-def philosopher(name, left_fork, right_fork):
-    print("philosopher ", name, " its in place")
-    think(name)
-    take_forks(name, left_fork, right_fork)
-    eat(name)
-    put_forks(name, left_fork, right_fork)
 
-N = 5
-names = ["Antonio", "Erick", "Isaque", "Ailson", "Stefano"]
-philosophers = []
+def take_forks(index, left_index, right_index):
+    condition.acquire()
+    while philosophers_states[left_index] == EATING or philosophers_states[right_index] == EATING:
+        condition.wait()
+    philosophers_states[index] = EATING
+    condition.release()
+
+
+def put_forks(index):
+    condition.acquire()
+    philosophers_states[index] = THINKING
+    condition.notify_all()
+    condition.release()
+
+
+def philosopher_run(index, left_index, right_index):
+    while True:
+        think(index)
+        take_forks(index, left_index, right_index)
+        eat()
+        put_forks(index)
+
 
 for i in range(N):
-    name = names[i]
-    left_fork = (i + N - 1) % N
-    right_fork = (i + 1) % N
-    philosopherX = Thread(target=philosopher, args=[name, left_fork, right_fork])
-    philosophers.append(philosopherX)
+    left_index = (i + N - 1) % N
+    right_index = (i + 1) % N
+    philosophers_threads.append(Thread(target=philosopher_run, args=[i, left_index, right_index]))
+    philosophers_states.append(THINKING)
+
+for philosopher in philosophers_threads:
+    philosopher.start()
+
+for philosopher in philosophers_threads:
+    philosopher.join()
